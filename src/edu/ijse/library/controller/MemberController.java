a5d30198df5da66817c759fa349be3b457fc15c0
Saneshka Costa <costasaneshka@outlook.com>
2024-07-05 17:19:09 +0530

    Project init

---
+++ src/edu/ijse/library/controller/MemberController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.library.controller;
+
+import edu.ijse.library.dto.MemberDTO;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hirus
+ */
+public class MemberController {
+    
+    public String save(MemberDTO dto) throws Exception{
+        return null;
+    }
+    public String update(MemberDTO dto) throws Exception{
+        return null;
+    }
+    public String delete(String code) throws Exception{
+        return null;
+    }
+    public MemberDTO get(String code) throws Exception{
+        return null;
+    }
+    public ArrayList<MemberDTO> getAll() throws Exception{
+        return null;
+    }
+}
---
+++ src/edu/ijse/library/dto/MemberDTO.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.library.dto;
+
+/**
+ *
+ * @author hirus
+ */
+public class MemberDTO {
+    private String mCode;
+    private String firstName;
+    private String lastName;
+    private  String phoneNo;
+    private String address;
+
+    public MemberDTO() {
+    }
+
+    public MemberDTO(String mCode, String firstName, String lastName, String phoneNo, String address) {
+        this.mCode = mCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNo = phoneNo;
+        this.address = address;
+    }
+    
+    
+
+    /**
+     * @return the mCode
+     */
+    public String getmCode() {
+        return mCode;
+    }
+
+    /**
+     * @param mCode the mCode to set
+     */
+    public void setmCode(String mCode) {
+        this.mCode = mCode;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the phoneNo
+     */
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    /**
+     * @param phoneNo the phoneNo to set
+     */
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDTO{" + "mCode=" + mCode + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNo=" + phoneNo + ", address=" + address + '}';
+    }
+    
+    
+}
